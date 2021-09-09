e49d7546d02b46df92cd2ef38e100c1af2de73e4
Bini <ccomangi2@gmail.com>
2021-09-09 13:57:15 +0900

    지도 띄우기

--- app/src/main/java/com/example/simsimhalmap/MainActivity.kt
+++ app/src/main/java/com/example/simsimhalmap/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.simsimhalmap
 
-import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Base64
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_main.*
+import net.daum.mf.map.api.MapView
 import java.security.MessageDigest
 
 class MainActivity : AppCompatActivity() {
@@ -15,19 +18,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        try {
-            val info = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
-            val signatures = info.signingInfo.apkContentsSigners
-            val md = MessageDigest.getInstance("SHA")
-            for (signature in signatures) {
-                val md: MessageDigest
-                md = MessageDigest.getInstance("SHA")
-                md.update(signature.toByteArray())
-                val key = String(Base64.encode(md.digest(), 0))
-                Log.d("Hash key:", "$key")
-            }
-        } catch(e: Exception) {
-            Log.e("name not found", e.toString())
-        }
+        val mapView = MapView(this)
+        val mapViewContainer = map_view as ViewGroup
+
+        mapViewContainer.addView(mapView)
     }
 }
