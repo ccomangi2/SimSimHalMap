f47153a3e5e13a0e63a38f9e6988c4c9fc4aa6c1
Bini <ccomangi2@gmail.com>
2021-09-09 13:35:51 +0900

    지도 SDK의 지도 타일은 http 통신을 하기 때문에 http 통신에 대한 예외 처리

---
+++ app/src/main/java/com/example/simsimhalmap/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.simsimhalmap
+
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import androidx.annotation.RequiresApi
+import java.security.MessageDigest
+
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        try {
+            val info = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
+            val signatures = info.signingInfo.apkContentsSigners
+            val md = MessageDigest.getInstance("SHA")
+            for (signature in signatures) {
+                val md: MessageDigest
+                md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val key = String(Base64.encode(md.digest(), 0))
+                Log.d("Hash key:", "$key")
+            }
+        } catch(e: Exception) {
+            Log.e("name not found", e.toString())
+        }
+    }
+}
